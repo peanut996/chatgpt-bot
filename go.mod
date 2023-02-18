f4b93c1b59a671d674edf74daca5a26bc05b4abd
Peanuts <849421294godw@gmail.com>
2023-02-18 10:06:23 +0000

    use singleleton chat bot

--- go.mod
+++ go.mod
@@ -3,12 +3,6 @@ module chatgpt-bot
 go 1.19
 
 require (
-	github.com/PullRequestInc/go-gpt3 v1.1.11
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
 	github.com/joho/godotenv v1.3.0
-)
-
-require (
-	github.com/stretchr/testify v1.8.1 // indirect
-	golang.org/x/net v0.4.0 // indirect
 )
--- logic/chatgpt.py
+++ logic/chatgpt.py
@@ -2,17 +2,24 @@ import os
 from revChatGPT.V1 import Chatbot
 
 
-def chat_with_chatgpt(sentence: str) -> str:
+bot: Chatbot = None
+
+def init_chatgpt():
+    global bot
     account = os.getenv('OPEN_AI_ACCOUNT')
     passwd = os.getenv('OPEN_AI_PASSWORD')
     conversation_id = os.getenv('OPEN_AI_CONVERSATION_ID')
-    chatbot = Chatbot(config={
+    bot = Chatbot(config={
         "email": account,
         "password": passwd
     }, conversation_id=conversation_id)
+
+def chat_with_chatgpt(sentence: str) -> str:
+    if bot is None:
+        init_chatgpt()
     res = ""
     prev_text = ""
-    for data in chatbot.ask(sentence):
+    for data in bot.ask(sentence):
         message = data["message"][len(prev_text):]
         res += message
         prev_text = data["message"]
