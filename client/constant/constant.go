6e1a78d727bbc116e1b61b9928a890c1f424d35d
peanut996 <849421294godw@gmail.com>
2023-02-23 11:09:40 +0800

    update user tip

--- client/constant/constant.go
+++ client/constant/constant.go
@@ -2,12 +2,12 @@ package constant
 
 var (
 	BotPingTip               = "pong"
-	UnknownCmdTip            = "Unknown command, please send /start to start a chat \n\n 未知命令，请发送 /start 来开始聊天"
-	BotStartTip              = "Hi, I'm ChatGPT bot. I can chat with you. Just send me a sentence and I will reply you. \n\n 请在这条消息下回复你的问题，我会回复你的😊"
-	OnlyOneChatAtATime       = "you are chatting with me, please wait for a while. \n\n 你已经发送了一条信息，请耐心等待😅"
-	LimitUserMessageTemplate = "You should join channel %s and group %s, then you can talk to me \n\n 你需要加入频道 %s 和群组 %s，然后才能和我交谈😢"
+	UnknownCmdTip            = "Unknown command, please send /start to start a chat \n\n🔥未知命令，请发送 /start 来开始聊天"
+	BotStartTip              = "Hi, I'm ChatGPT bot. I can chat with you. Just send me a sentence and I will reply you. \n\n😊请在这条消息下回复你的问题，我会回复你的"
+	OnlyOneChatAtATime       = "you are chatting with me, please wait for a while. \n\n😅你已经发送了一条信息，请耐心等待"
+	LimitUserMessageTemplate = "You should join channel %s and group %s, then you can talk to me \n\n😢你需要加入频道 %s 和群组 %s，然后才能和我交谈"
 
-	ChatGPTError           = "ChatGPT return error, try later again \n\n 出错了, 稍后重试下吧😅"
-	ChatGPTErrorTemplate   = "ChatGPT return error, try later again \n\n 出错了, 稍后重试下吧😅 \n\n %s"
-	ChatGPTEngineNotOnline = "Chatgpt engine is not ready, please wait a moment. \n\n ChatGPT 引擎还没有准备好，请稍等一下😅"
+	ChatGPTError           = "ChatGPT return error, try later again \n\n😇出错了, 稍后重试下吧"
+	ChatGPTErrorTemplate   = "ChatGPT return error, try later again \n\n😇出错了, 稍后重试下吧 \n\n %s"
+	ChatGPTEngineNotOnline = "Chatgpt engine is not ready, please wait a moment. \n\n😇ChatGPT 引擎还没有准备好，请稍等一下"
 )
