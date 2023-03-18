f85dd9e360861cc929eaaa5d34d40e08c31e3bd7
peanut996 <849421294godw@gmail.com>
2023-03-18 21:59:15 +0800

    [feat] invite user must be a new user.

--- client/bot/telegram/bot_telegram_cmd_handler.go
+++ client/bot/telegram/bot_telegram_cmd_handler.go
@@ -59,6 +59,15 @@ func (c *StartCommandHandler) handleInvitation(inviteCode string, inviteUserID s
 		log.Printf("[handleInvitation] find user by invite code failed, user is nil")
 		return errors.New("no such user by invite code: " + inviteCode)
 	}
+	inviteUser, err := c.userRepository.GetByUserID(inviteUserID)
+	if err != nil {
+		log.Printf("[handleInvitation] find user by invite user id failed, err: 【%s】", err)
+		return err
+	}
+	if inviteUser != nil {
+		log.Printf("[handleInvitation]  user has been invited by other user" + inviteUserID)
+		return nil
+	}
 	inviteRecord := persist.NewUserInviteRecord(user.UserID, inviteUserID)
 	err = c.userInviteRecordRepository.Insert(inviteRecord)
 	if err != nil {
