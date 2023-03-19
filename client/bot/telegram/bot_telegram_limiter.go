5baca2972bf04df695191d2c672c7e9737b45187
peanut996 <849421294godw@gmail.com>
2023-03-20 02:59:01 +0800

    [feat] rate limiter for all user.

--- client/bot/telegram/bot_telegram_limiter.go
+++ client/bot/telegram/bot_telegram_limiter.go
@@ -182,7 +182,7 @@ func NewRateLimiter(capacity int64, duration int64) *RateLimiter {
 }
 
 func (r *RateLimiter) Allow(bot *Bot, message tgbotapi.Message) (bool, string) {
-	if !IsGPT4Message(message) || !bot.enableLimiter {
+	if !bot.enableLimiter {
 		return true, ""
 	}
 	if !r.limiter.Allow(strconv.FormatInt(message.From.ID, 10)) {
