86a20d2b690522f52db1e12449e2e33342bc6989
Peanuts <849421294godw@gmail.com>
2023-03-19 07:06:10 +0000

    [feat] get conversation id for each user

--- engine/session/user_session.py
+++ engine/session/user_session.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import time
 from .credential import Credential
 
 
@@ -8,10 +8,10 @@ class UserSession:
         self.user_id = user_id
         self.conversation_id = conversation_id
         self.parent_id = parent_id
-        self.last_time = datetime.now()
+        self.last_time = time.time()
         self.credential = credential
 
     def update(self, parent_id=None, conversation_id=None):
-        self.last_time = datetime.now()
+        self.last_time = time.time()
         self.parent_id = parent_id
         self.conversation_id = conversation_id or self.conversation_id
