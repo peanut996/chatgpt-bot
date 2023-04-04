5a8b0c87eb5c36b463a41d2d3fd78b3ea0a29c99
peanut996 <849421294godw@gmail.com>
2023-04-04 19:57:51 +0800

    [chore] use md format

--- client/constant/tip/tip.go
+++ client/constant/tip/tip.go
@@ -51,6 +51,6 @@ var (
 	QueueTipTemplate = "Current queue count: %d, please wait wait patiently\n\n" +
 		"🔥当前排队人数：%d, 请耐心等候\n\n"
 
-	AccessCodeTipTemplate = "Your access code is: %s\n\n" +
-		"😊你的访问码是: %s\n\n"
+	AccessCodeTipTemplate = "Your access code: `%s`\n\n" +
+		"😊你的访问码是: `%s`\n\n"
 )
